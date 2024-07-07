4d5befd0c03cfb9a80c9b8801c50f4b92d4a7802
Serag Sorror <serag.sorror@gmail.com>
2024-07-06 18:09:09 -0700

    Removed unused comments

--- ipc_socketpair.c
+++ ipc_socketpair.c
@@ -8,8 +8,6 @@
 #define MAX_RETRIES 3
 
 int main() {
-    //int beginTime = clock();
-
     int sv[2];//socket pair file descriptors
     if(socketpair(AF_UNIX, SOCK_STREAM, 0, sv) == -1) {//Creates pair of connected sockets 
         perror("socketpair");
@@ -30,22 +28,10 @@ int main() {
         struct IPCDataStruct dataToSend[2] = {
             {1, 1.1f, "sampleString1", type1}, {2, 2.1f, "sampleString2", type2}
         };
-
-        // for (int i = 0; i < NUM_MESSAGES_TO_SEND; ++i) {
-        //     ssize_t bytes_sent = write(sv[1], &dataToSend[i], sizeof(dataToSend[i])); //was facing partial write issues where nhot all bytes of struct were written to socket,
-        //                                                     //could be due to various reasons such as buffer size or interruptions in communication.
-        //                                                     //SOLUTION: Added check after each 'write' operation to verify exact number of bytes from each array index in 'dataToSend' was successfully written
-
-        //                                                     //ISSUE ALSO COULD HAVE BEEN EITHER 1. SIZE MISMATCH, 2. ALIGNMENT AND PADDING CAUSING MISINTERPRETATION, 3. READING LOGIC(READ CORRECT NUMBER OF BYTES)
-        //     if (bytes_sent == -1) {
-        //         perror("write");
-        //         close(sv[1]);
-        //         exit(EXIT_FAILURE);
-        //     } else if(bytes_sent != sizeof(dataToSend[i])) {
-        //         fprintf(stderr, "ERROR: Incomplete/Partial write occured\n");
-        //         close(sv[1]);
-        //         exit(EXIT_FAILURE);
-        //     }
+    //was facing partial write issues where not all bytes of struct were written to socket,
+    //could be due to various reasons such as buffer size or interruptions in communication.
+    //SOLUTION: Added check after each 'write' operation to verify exact number of bytes from each array index in 'dataToSend' was successfully written and add a retry
+    //ISSUE ALSO COULD HAVE BEEN EITHER 1. SIZE MISMATCH, 2. ALIGNMENT AND PADDING CAUSING MISINTERPRETATION, 3. READING LOGIC(READ CORRECT NUMBER OF BYTES)
 
 for (int i = 0; i < NUM_MESSAGES_TO_SEND; ++i) {
             int retries = 0;
@@ -72,48 +58,11 @@ for (int i = 0; i < NUM_MESSAGES_TO_SEND; ++i) {
             }
         }
         //}
-        
 
         close(sv[1]);
         exit(EXIT_SUCCESS);
     } else { //pid must be 1 aka parent process - Receiver
         close(sv[1]); //close unused process
-
-    //     struct DataStruct receivedData[NUM_MESSAGES_TO_SEND];
-
-
-    //     for (int i = 0; i < NUM_MESSAGES_TO_SEND; ++i) {
-    //         ssize_t bytes_received = read(sv[0], &receivedData[i], sizeof(receivedData[i]));
-    //         if (bytes_received == -1) {
-    //             perror("read");
-    //             close(sv[0]);
-    //             exit(EXIT_FAILURE);
-    //         } else if (bytes_received != sizeof(receivedData[i])) {
-    //             fprintf(stderr, "Incomplete/Partial read occured");
-    //             close(sv[0]);
-    //             exit(EXIT_FAILURE);
-    //         }
-            
-    //         const char *typeString; //CHANGE NAME
-    //         switch (receivedData[i].type) {
-    //             case type1: typeString = "type1"; break;
-    //             case type2: typeString = "type2"; break;
-    //             case type3: typeString = "type3"; break;
-    //             default: typeString = "unknown"; break;
-    //         }
-    //         printf("\n--Next Message--\n");
-    //         //Print 10 diffrent versions of received data
-    //         for (int j = 0; j < 10; ++j) {
-    //             char combinedString[25];
-    //             sprintf(combinedString, "%s_%d", receivedData[i].str, j);
-    //             printf("integer: %d, float: %.2f, string: %s, type: %s\n",
-    //             receivedData[i].num + j,
-    //             receivedData[i].fl + j * 0.1f,
-    //             combinedString,
-    //             typeString);
-    //         }
-            
-    //    }
 
     struct IPCDataStruct receivedData;
     for (int i = 0; i < NUM_MESSAGES_TO_SEND; ++i) {
@@ -150,7 +99,6 @@ for (int i = 0; i < NUM_MESSAGES_TO_SEND; ++i) {
         close(sv[0]);
         wait(NULL); //wait for child process to finish
     }
-    // int end = clock();
-    // printf("execution time was: %d ticks or %.2f seconds", end - beginTime, ((float)end - beginTime)/CLOCKS_PER_SEC);
+
     return 0;
 }
